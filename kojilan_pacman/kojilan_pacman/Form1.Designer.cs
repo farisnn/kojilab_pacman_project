a5fa184d2cdc590a2bdd4fda29a2a918d114f274
kojiri <kojiri@DESKTOP-B2MO7MA>
2017-12-01 17:00:30 +0900

    zeif

--- kojilan_pacman/kojilan_pacman/Form1.Designer.cs
+++ kojilan_pacman/kojilan_pacman/Form1.Designer.cs
@@ -29,26 +29,38 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(34, 62);
+            this.button1.Location = new System.Drawing.Point(16, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(392, 132);
+            this.button1.Size = new System.Drawing.Size(181, 66);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(133, 162);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 277);
+            this.panel1.TabIndex = 1;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1562, 1117);
+            this.ClientSize = new System.Drawing.Size(721, 502);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- kojilan_pacman/kojilan_pacman/Form1.cs
+++ kojilan_pacman/kojilan_pacman/Form1.cs
@@ -24,6 +24,41 @@ namespace kojilan_pacman
         {
 
         }
+
+        private System.Windows.Forms.PictureBox[] testPicureBox;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.testPicureBox = new System.Windows.Forms.PictureBox[418];
+            int i = 0;
+            for (int y = 0; y < 23; y++)
+            {
+                
+                for(int x=0;x<20;x++)
+                {
+                    i++;
+                    this.testPicureBox[i] = new System.Windows.Forms.PictureBox();
+                    this.testPicureBox[i].Size = new Size(100, 100);
+                    this.testPicureBox[i].Location = new Point( x * 100, y * 100);
+                    if (Map[x][y] == 0)
+                    {
+                        this.testPicureBox[i].Image = Image.FromFile(@"C:\Documents and Settings\hogehoge\My Documents\My Pictures\00.gif");
+                    }
+                    if (Map[x][y] == 1)
+                    {
+                        this.testPicureBox[i].Image = Image.FromFile(@"C:\Documents and Settings\hogehoge\My Documents\My Pictures\01.gif");
+                    }
+                    if (Map[x][y] == 2)
+                    {
+                        this.testPicureBox[i].Image = Image.FromFile(@"C:\Documents and Settings\hogehoge\My Documents\My Pictures\02.gif");
+                    }
+                       
+
+                    this.Controls.Add(testPicureBox[i]);
+                }
+               
+            }
+
+        }
     }
-    
-}
+  
